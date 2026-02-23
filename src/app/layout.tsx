5223a9a839adef02acb435776a36574140a2e1ba
IntStrFloat <gostlix20201@gmail.com>
2026-02-23 21:23:13 +0300

    Update Yandex.Metrika integration in layout component
    - Revised the Yandex.Metrika script to include the correct tracking ID and enhanced configuration options for improved tracking capabilities.
    - Updated the image source for Yandex.Metrika to reflect the new tracking ID.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -128,16 +128,21 @@ export default function RootLayout({
         {/* Яндекс.Метрика */}
         <Script id="yandex-metrika" strategy="afterInteractive">
           {`
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-            ym(XXXXXXXXX, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=106971287", "ym");
+            ym(106971287, "init", {
+              ssr: true,
+              webvisor: true,
+              clickmap: true,
+              ecommerce: "dataLayer",
+              referrer: document.referrer,
+              url: location.href,
+              accurateTrackBounce: true,
+              trackLinks: true
             });
           `}
         </Script>
@@ -145,7 +150,7 @@ export default function RootLayout({
           <div>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              src="https://mc.yandex.ru/watch/XXXXXXXXX"
+              src="https://mc.yandex.ru/watch/106971287"
               style={{ position: "absolute", left: "-9999px" }}
               alt=""
             />
